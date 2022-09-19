cb4e5186bf3ced3c89400d94c9eea0d90af109bc
haris036 <109869504+haris036@users.noreply.github.com>
2022-09-20 03:56:16 +0500

    fixes listing

--- listing.js
+++ listing.js
@@ -128,7 +128,7 @@ method.getListing = async function (searchKeyWord) {
         for (let i = 0; i < item.materials.length; i++) {
           if (!material_wise_items_map.has(item.materials[i].toLowerCase())) {
             material_wise_items_map.set(item.materials[i].toLowerCase(), material_item = {
-              
+
               category_wise_map: new Map(),
             });
           }
@@ -244,16 +244,15 @@ method.getListing = async function (searchKeyWord) {
       let shipping_days = Array.from(shipping_days_map.entries());
       let shipping_prices = Array.from(shipping_prices_map.entries());
       let long_tail_alternatives = Array.from(long_tail_alternatives_map.entries());
-      const toObject = (map = new Map) =>
-        Object.fromEntries
-          (Array.from
-            (material_wise_items_map.entries()
-              , ([k, v]) =>
-                v instanceof Map
-                  ? [k, toObject(v)]
-                  : [k, v]
-            )
-          )
+      let material_items = new Map()
+      for (let k of Object.keys(material_wise_items_map)) {
+        if (material_wise_items_map[k] instanceof Object) {
+          material_items.set(k, objectToMap(material_wise_items_map[k]))
+        }
+        else {
+          material_items.set(k, o[k])
+        }
+      }
       // let material_items = Array.from(material_wise_items_map.entries());
       let similar_shopper_searches = Array.from(similar_shopper_searches_map.entries());
       let result = {
