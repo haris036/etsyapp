5fd21e0ed8a280dba430e54c69d86f8f8dbd9cfc
haris036 <109869504+haris036@users.noreply.github.com>
2022-11-12 22:12:52 +0500

    changes in save user to handle duplication error

--- login_or_signup.js
+++ login_or_signup.js
@@ -16,25 +16,31 @@ let uri =
 const client = new MongoClient(uri);
 
 
-method.saveUser = async function ( _email,  _password, _is_subscribed) {
+method.saveUser = async function (_email, _password, _is_subscribed) {
   let usr = "";
-  let insert_id ="";
+  let insert_id = "";
   try {
 
     await client.connect();
-    usr = _email.substr(0,_email.indexOf('@'));
+    usr = _email.substr(0, _email.indexOf('@'));
     const database = client.db("etsy_database");
     console.log(usr)
     const collection = database.collection("user_data");
 
     const doc = { user: usr, password: _password, creation_time: Date.now(), email: _email, is_subscribed: _is_subscribed, last_updated: null, expiry: Date.now() + 2592000 };
     const result = await collection.insertOne(doc);
+
     console.log(
       `A document was inserted with the _id: ${result.insertedId}`,
     );
     insert_id = result.insertedId;
   } catch (e) {
-    console.log(e)
+    if ( e.code === 11000) {
+      return "User already exists with same email id"
+    }
+    else {
+      return (e)
+    }
   } finally {
 
     await client.close();
@@ -59,11 +65,9 @@ method.updateUserPassword = async function (_email, _password) {
     await dbo.collection("user_data").updateOne(myquery, newvalues);
 
   } catch (e) {
-    console.log(e)
+    return e
   } finally {
-
     await client.close();
-
   }
   let response = {
     msg: "Updated",
@@ -76,14 +80,12 @@ method.updateSubscription = async function (_email, _is_subscribed) {
   try {
 
     await client.connect();
-
     var dbo = client.db("etsy_database");
     var myquery = { email: _email };
     var newvalues = { $set: { is_subscribed: _is_subscribed, last_updated: Date.now() } };
     await dbo.collection("user_data").updateOne(myquery, newvalues);
-
   } catch (e) {
-    console.log(e)
+    return e
   } finally {
 
     await client.close();
@@ -108,14 +110,15 @@ method.getUser = async function (_email) {
     const cursor = ratings.find({ email: _email });
 
     await cursor.forEach(doc => user_data.push(doc));
-    
+
   } catch (e) {
-    console.log(e)
+    return e
   } finally {
 
     await client.close();
 
   }
+
   return user_data;
 }
 
