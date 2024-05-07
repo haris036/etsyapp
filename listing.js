4a253f420aaadaa7b8580f7699ff61dba53f2a7f
haris036 <109869504+haris036@users.noreply.github.com>
2024-05-07 18:48:52 +0500

    Update listing.js

--- listing.js
+++ listing.js
@@ -457,11 +457,10 @@ method.getListing = async function (searchKeyWord, email, is_single_listing, api
       
       if (!is_single_listing) {
         await client.connect();
-        console.log("Harus" + email.user);
         const database = client.db("etsy_database");
 
         const doc = {
-          email: email.user,
+          email: email,
           keyword: searchKeyWord,
           searches: result.searches,
           competition: result.competition,
